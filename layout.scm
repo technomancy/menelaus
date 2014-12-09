5f629927fbc1c2b601cf36d41eaad3b89bd73c55
Phil Hagelberg <phil@hagelb.org>
2014-12-08 23:09:13 -0800

    Scan the whole matrix.

---
+++ layout.scm
@@ -0,0 +1,9 @@
+(define layout
+  (list (list key-q key-w key-e key-r key-t 0
+              key-y key-u key-i key-o key-p)
+        (list key-a key-s key-d key-f key-g 0
+              key-h key-j key-k key-l key-semicolon)
+        (list key-z key-x key-c key-v key-b mod-alt
+              key-n key-m key-comma key-period key-slash)
+        (list key-esc key-tab mod-gui mod-shift key-backspace mod-ctrl
+              key-space mod-fn key-dash key-quote key-enter)))
--- menelaus.scm
+++ menelaus.scm
@@ -1,22 +1,29 @@
-;; -*- scheme -*-
-
 (include "keycodes.scm")
+(include "layout.scm")
 
-(define column-pins (list 11 12 18 19 10 7 8 9 5 6))
-(define layout (list key-a key-s key-d key-f key-g
-                     key-h key-j key-k key-l key-semicolon))
+(define row-pins (list 0 1 2 3))
+(define column-pins (list 11 12 18 19 10 4 7 8 9 5 6))
 
-(define (scan-column column-pins layout pressed)
-  (if (null? column-pins)
+(define (scan-column columns row-layout pressed)
+  (if (null? columns)
       pressed
-      (scan-column (cdr column-pins)
-                   (cdr layout)
-                   (if (low? (car column-pins))
-                       (car layout)
+      (scan-column (cdr columns)
+                   (cdr row-layout)
+                   (if (low? (car columns))
+                       (car row-layout)
                        pressed))))
 
+(define (scan-rows rows layout-rows pressed)
+  (if (null? rows)
+      pressed
+      (begin
+        (for-each high row-pins)
+        (low (car rows)) ; activate row
+        (scan-rows (cdr rows) (cdr layout-rows)
+                   (scan-column column-pins (car layout-rows) 0)))))
+
 (define (loop)
-  (let ((pressed (scan-row column-pins layout 0)))
+  (let ((pressed (scan-rows row-pins layout 0)))
     (call-c-func "usb_send" 0 pressed 0 0 0 0 0))
   (loop))
 
@@ -27,8 +34,7 @@
                               #x23 #x23 #x23 #x29 #x2D #x2D))
   (set! arduino-pins (vector 4 8 2 1 16 64 128 64 16 32 64 128 64 128
                              8 2 4 32 65 128))
-  (output 1)
-  (low 1)
+  (for-each output row-pins)
   (for-each input column-pins)
   (for-each high column-pins) ; activate pullup resistors
   (call-c-func "usb_init")
@@ -36,4 +42,3 @@
 
 (init)
 (loop)
-
